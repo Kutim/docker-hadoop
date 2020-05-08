9f2d34c16e9cfc4a5ccd7806a4e4e965cc52632f
Kutim <1252900197@qq.com>
2020-05-08 17:08:19 +0800

    hdfs 变为 2个NameNode，1个DataNode，1个journalnode 同时启动其他节点

--- namenode/run.sh
+++ namenode/run.sh
@@ -7,13 +7,16 @@ count=`ls $namedir | wc -l`
 
 if [ $count -eq 0 ]
 then
-    hdfs zkfc -formatZK
+    hdfs zkfc -formatZK -force
     echo "name node format."
-    dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -format -nonInteractive
+    # dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -format -nonInteractive
+    dockerize -wait tcp://journalnode1:8485 hdfs namenode -format -nonInteractive
+
 else
     hdfs --daemon stop namenode
     echo "name node bootstrapStandby."
-    dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -bootstrapStandby -nonInteractive
+    # dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -bootstrapStandby -nonInteractive
+    dockerize -wait tcp://journalnode1:8485 hdfs namenode -bootstrapStandby -nonInteractive
 fi
 
 # 启动 namenode 服务
@@ -25,4 +28,5 @@ echo "starting namenode servie done."
 
 hdfs --daemon stop zkfc
 echo "starting zkfc servie..."
-dockerize -wait tcp://namenode1:9870 -wait tcp://namenode2:9870 -wait tcp://namenode3:9870  hdfs  zkfc
+# dockerize -wait tcp://namenode1:9870 -wait tcp://namenode2:9870 -wait tcp://namenode3:9870  hdfs  zkfc
+dockerize -wait tcp://namenode1:9870  -wait tcp://namenode2:9870 hdfs  zkfc
--- namenode_standy/run.sh
+++ namenode_standy/run.sh
@@ -3,7 +3,8 @@
 
 
 echo "name node bootstrapStandby."
-dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -bootstrapStandby -nonInteractive
+# dockerize -wait tcp://journalnode1:8485 -wait tcp://journalnode2:8485 -wait tcp://journalnode3:8485 hdfs namenode -bootstrapStandby -nonInteractive
+dockerize -wait tcp://journalnode1:8485 hdfs namenode -bootstrapStandby -nonInteractive
 
 # 启动 namenode 服务
 hdfs --daemon stop namenode
@@ -13,4 +14,5 @@ echo "starting namenode servie done."
 
 hdfs --daemon stop zkfc
 echo "starting zkfc servie..."
-dockerize -wait tcp://namenode1:9870 -wait tcp://namenode2:9870 -wait tcp://namenode3:9870  hdfs zkfc
+# dockerize -wait tcp://namenode1:9870 -wait tcp://namenode2:9870 -wait tcp://namenode3:9870  hdfs zkfc
+dockerize -wait tcp://namenode1:9870  -wait tcp://namenode2:9870 hdfs zkfc
